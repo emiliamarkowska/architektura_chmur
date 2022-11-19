b9d6526a8591da7bc93d71d5fcafa7769d722b87
student <student@L407-7.helium.com>
2022-11-19 11:26:37 +0100

    reversing string workds

--- lab2/source/src/main/java/com/architektura/chmur/lab2/Controller.java
+++ lab2/source/src/main/java/com/architektura/chmur/lab2/Controller.java
@@ -1,6 +1,7 @@
 package com.architektura.chmur.lab2;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -22,5 +23,10 @@ public class Controller {
         ZoneId zoneId = ZoneId.of("Europe/Warsaw");
         ZonedDateTime now = LocalDateTime.now().atZone(zoneId);
         return dtf.format(now);
+    }
+
+    @GetMapping(path = "/rev/{text}")
+    public String reverseString(@PathVariable String text) {
+        return new StringBuilder(text).reverse().toString();
     }
 }
